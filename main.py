388ed16bb97ccd0fa3be2f173c821b5fa0804fca
Paulo Henrique <gladgamer01@gmail.com>
2019-04-12 09:30:41 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as cp
 import pytesseract as ocr
-from PIL
-import Image
+from PIL import Image
 
-ocr.pytesseract.tesseract_cmd = r "PATH TESSERACT HERE"
 
-phrase = ocr.image_to_string(Image.open('website.png'), lang = 'eng')
+ocr.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+phrase = ocr.image_to_string(Image.open('C:\\Users\\Paulo Henrique\\Desktop\\ImageToWebSite\\website.png'), lang='eng')
 
 css_codes = phrase.split(";")
 debug = []
@@ -14,34 +14,44 @@ execute = []
 css = []
 
 for i in css_codes:
-  if i is not "":
-  if "\n" in i:
-  debug.append(i.replace("\n", ""))
-else :
-  debug.append(i)
+	if i is not "":
+		if "\n" in i:
+			debug.append(i.replace("\n",""))
+		else:
+			debug.append(i)
 
 def detectColor(input_):
-  colors = ['red', 'blue']
-if input_ in colors:
-  return True
-else :
-  return False
+	colors = ['red','blue']
+	if input_ in colors:
+		return True
+	else:
+		return False
 
-def __interpreter(debug, css):
-  if (debug is None):
-    print("Error to read debug")
-else :
-  for i in range(len(debug)):
-  if "#" in debug[i]:
-  memory = debug[i].split(" ")
-isAColor = detectColor(memory[1])
-if isAColor == True:
-  css.append(f "{memory[0]} " + "{" + f "color: {memory[1]}"
-    "}")
+def detectSizeModify(input_):
+	if "pt" in input_:
+		return True
+	else:
+		return False
 
-__interpreter(debug, css)
-f = open("main.css", "w+")
+def __interpreter(debug,css):
+	if (debug is None):
+		print("Error to read debug")
+	else:
+		for i in range(len(debug)):
+				if "#" in debug[i]:
+					memory = debug[i].split(" ")
+					isAColor = detectColor(memory[1])
+					isASizeModify = detectSizeModify(memory[1])
+					if isASizeModify == True:
+						css.append(f"{memory[0]} " + "{" + f"font-size: {memory[1]}""}")
+					if isAColor == True:
+						css.append(f"{memory[0]} " + "{" + f"color: {memory[1]}""}")
+
+
+
+__interpreter(debug,css)
+f = open("main.css","w+")
 for i in css:
-  f.write(f "{i}\r\n")
+	f.write(f"{i}\r\n")
 f.close()
-print(css)
+print(css) 
