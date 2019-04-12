acb0042b83b8ba4f833744d6dee52bfdb840bf8d
Paulo Henrique <gladgamer01@gmail.com>
2019-04-12 09:57:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 ocr.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-phrase = ocr.image_to_string(Image.open('C:\\Users\\Paulo Henrique\\Desktop\\ImageToWebSite\\website.png'), lang='eng')
+phrase = ocr.image_to_string(Image.open('website.png'), lang='eng')
 
 css_codes = phrase.split(";")
 debug = []
@@ -33,7 +33,20 @@ def detectSizeModify(input_):
 	else:
 		return False
 
+def detectHoverStart(input_):
+	if "@hover" in input_:
+		return True
+	else:
+		return False
+
+def detectHoverEnd(input_):
+	if "hover@" in input_:
+		return True
+	else:
+		return False
+
 def __interpreter(debug,css):
+	inhover = False
 	if (debug is None):
 		print("Error to read debug")
 	else:
@@ -42,10 +55,24 @@ def __interpreter(debug,css):
 					memory = debug[i].split(" ")
 					isAColor = detectColor(memory[1])
 					isASizeModify = detectSizeModify(memory[1])
+					isAStartHover = detectHoverStart(memory[1])
+					isaEndHover = detectHoverEnd(memory[1])
+					if isAStartHover == True:
+						inhover = True
+						css.append(f"{memory[0]}:hover " + "{")
+					if isaEndHover == True:
+						inhover = False
+						css.append("}")
 					if isASizeModify == True:
-						css.append(f"{memory[0]} " + "{" + f"font-size: {memory[1]}""}")
+						if inhover == False:
+							css.append(f"{memory[0]} " + "{" + f"font-size: {memory[1]}""}")
+						else:
+							css.append(f"font-size: {memory[1]}")
 					if isAColor == True:
-						css.append(f"{memory[0]} " + "{" + f"color: {memory[1]}""}")
+						if inhover == False:
+							css.append(f"{memory[0]} " + "{" + f"color: {memory[1]}""}")
+						else:
+							css.append(f"color: {memory[1]}")
 
 
 
